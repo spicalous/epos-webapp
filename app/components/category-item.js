c55e10a48aa74a7bed65c269abcaa3a14f24b145
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-15 00:50:46 +0100

    some css

--- app/components/category-item.js
+++ app/components/category-item.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
+  classNames: ['col-xs-3', 'col-md-3'],
   click: function() {
     this.sendAction('action', this.get('category'));
   }
--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
+    classNames: ['list-group-item'],
   click: function() {
     this.sendAction('action', this.get('menuItem'));
   }
