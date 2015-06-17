0345a324c3d0937928c52a2f078ad3dedfb92fc9
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-18 00:22:42 +0100

    Redundant menu

--- app/pods/menu/route.js
+++ app/pods/menu/route.js
@@ -1,7 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('menu-item');
-  }
-});
+export default Ember.Route.extend({});
