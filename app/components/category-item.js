7a2bc11f9c93417f99c369c3f83f94b82ac90771
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-18 23:41:25 +0100

    Split out css

--- app/components/category-item.js
+++ app/components/category-item.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   tagName: 'button',
   attributeBindings: ['type'],
   type: 'button',
-  classNames: ['col-xs-3', 'btn', 'btn-default', 'category-item-btn'],
+  classNames: ['col-xs-3', 'btn', 'btn-default', 'category-item'],
   click: function() {
     this.sendAction('action', this.get('category'));
   }
