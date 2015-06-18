dfc4acdac176a30263ec4a04ced6b44f6878d0e6
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-18 23:56:10 +0100

    redundant css

--- Brocfile.js
+++ Brocfile.js
@@ -17,10 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-    destDir: 'assets'
-});
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
     destDir: 'fonts'
 });
