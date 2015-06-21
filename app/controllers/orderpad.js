9d538e426acd1b01a442b0923e1406778c81d30c
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-21 19:42:33 +0100

    TODO: filter by category

--- app/controllers/orderpad.js
+++ app/controllers/orderpad.js
@@ -3,20 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     categoryItemClick: function(categoryItem) {
-
+      //TODO Filter menu model by selecting category
     },
     submitOrder: function() {
       var _this = this;
 
       this.get('model').order
-        .set('date', Date.now())
-        .save()
-        .then(function() {
-          //TODO Handle fail scenarios
-        }, function() {
-          //TODO Find a way to refresh just model.order otherwise a REST call is made for a non-changing menu
-          _this.send('refresh');
-        });
+          .set('date', Date.now())
+          .save()
+          .then(function() {
+            //TODO Handle fail scenarios
+          }, function() {
+            //TODO Find a way to refresh just model.order otherwise a REST call is made for a non-changing menu
+            _this.send('refresh');
+          });
     }
   }
 });
