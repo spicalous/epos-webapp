49ad43802d2ace2ee567b2af681278b6140b1cc2
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-06-22 18:56:59 +0100

    no message

--- app/components/order-item.js
+++ app/components/order-item.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  tagName: 'li',
+  tagName: 'a',
   classNames: ['list-group-item'],
   click: function() {
     this.sendAction('action', this.get('orderItem'));
