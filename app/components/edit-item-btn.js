71b2362ea93088efa490bca0b8ef04eedf3bcf0a
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-05 23:58:50 +0100

    renamed

--- app/components/edit-item-btn.js
+++ app/components/edit-item-btn.js
@@ -3,8 +3,5 @@ import Ember from "ember";
 export default Ember.Component.extend({
   tagName: 'a',
   attributeBindings: ['data-toggle', 'aria-haspopup', 'aria-expanded'],
-  classNames: ['edit-item-btn', 'dropdown-toggle'],
-  click: function() {
-    this.sendAction('action');
-  }
+  classNames: ['edit-item-btn', 'dropdown-toggle']
 });
