ee3da3652b339b60f1b827e72541cc24902dff73
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-09 00:08:55 +0100

    aggregates edit components

--- app/pods/components/edit/edit-btn/component.js
+++ app/pods/components/edit/edit-btn/component.js
@@ -1,9 +1,18 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  tagName: 'a',
-  attributeBindings: ['dataToggle:data-toggle', 'ariaHaspopup:aria-haspopup'],
-  dataToggle: 'dropdown',
-  ariaHaspopup: true,
-  classNames: ['edit-item-btn', 'dropdown-toggle']
+  tagName: 'span',
+  drinkOptions: Ember.A(['Edit Drink']),
+  foodOptions: Ember.A(['Add Food', 'Edit Food']),
+  desertOptions: Ember.A(['Add Desert', 'Edit Dessert']),
+  editItemOptions: function() {
+    var categories = this.get('orderItem')
+                         .get('menuItem')
+                         .get('categories');
+
+    switch(categories.objectAt(0).get('id')) {
+      case "16": return this.get('drinkOptions');
+      default: return this.get('foodOptions');
+    }
+  }.property('categories')
 });
