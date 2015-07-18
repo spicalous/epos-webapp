5de77aed3132261dfd94c73dd7e4f9459ea4a88a
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-18 15:32:22 +0100

    fixed tests

--- tests/acceptance/orderpad-test.js
+++ tests/acceptance/orderpad-test.js
@@ -18,8 +18,8 @@ test("Page contents", function(assert) {
 
   assert.expect(2);
   visit('/orderpad/restaurant').then(function() {
-    assert.equal(find('.orderpad-menu .list-group-item').length, 129, "Correct number of menu items");
-    assert.equal(find('.orderpad-categories .row button').length, 17, "Correct number of category items");
+    assert.equal(find('#orderpad-menu .list-group-item').length, 129, "Correct number of menu items");
+    assert.equal(find('#orderpad-categories .row button').length, 17, "Correct number of category items");
   });
 
 });
@@ -28,12 +28,12 @@ test("Adding an item in the main view", function(assert) {
 
   assert.expect(3);
   visit('/orderpad/restaurant');
-  click('.orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
 
   andThen(function() {
     assert.equal(find('#orderpad-order .orderpad-order .list-group-item').length, 1, "Correctly added menu item to main order item list");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
   });
 
 });
@@ -42,13 +42,13 @@ test("Adding an item in the modal", function(assert) {
 
   assert.expect(3);
   visit('/orderpad/restaurant');
-  click('.orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
 
   andThen(function() {
 
     assert.equal(find('#orderpad-modal .orderpad-order .list-group-item').length, 1, "Correctly added menu item to modal order item list");
-    assert.ok(find('#orderpad-modal .orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
-    assert.ok(find('#orderpad-modal .orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
+    assert.ok(find('#orderpad-modal #orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
+    assert.ok(find('#orderpad-modal #orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
   });
 
 });
@@ -57,11 +57,11 @@ test("Selecting a category", function(assert) {
 
   assert.expect(1);
   visit('/orderpad/restaurant');
-  click('.orderpad-categories button:first-child');
+  click('#orderpad-categories button:first-child');
 
   andThen(function() {
 
-    assert.equal(find('.orderpad-menu .list-group-item').length, 18, "Correct number of menu items after filtering");
+    assert.equal(find('#orderpad-menu .list-group-item').length, 18, "Correct number of menu items after filtering");
   });
 
 });
@@ -70,14 +70,14 @@ test("Deleting an order item with quantity of 1 removes it", function(assert) {
 
   assert.expect(3);
   visit('/orderpad/restaurant');
-  click('.orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
   click('#orderpad-order .orderpad-order .decrement-btn');
 
   andThen(function() {
 
     assert.equal(find('#orderpad-order .orderpad-order .list-group-item').length, 0, "Correctly removes menu item from main order item list");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('ITEMS: 0'), "Correctly updates the item count text");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('TOTAL: 0.00'), "Correctly updates the total text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('ITEMS: 0'), "Correctly updates the item count text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('TOTAL: 0.00'), "Correctly updates the total text");
   });
 
 });
@@ -86,14 +86,14 @@ test("Deleting an order item with quantity of 1 removes it", function(assert) {
 
   assert.expect(3);
   visit('/orderpad/restaurant');
-  click('.orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
   click('#orderpad-modal .orderpad-order .decrement-btn');
 
   andThen(function() {
 
     assert.equal(find('#orderpad-modal .orderpad-order .list-group-item').length, 0, "Correctly removes menu item from modal order item list");
-    assert.ok(find('#orderpad-modal .orderpad-summary').text().includes('ITEMS: 0'), "Correctly updates the item count text");
-    assert.ok(find('#orderpad-modal .orderpad-summary').text().includes('TOTAL: 0.00'), "Correctly updates the total text");
+    assert.ok(find('#orderpad-modal #orderpad-summary').text().includes('ITEMS: 0'), "Correctly updates the item count text");
+    assert.ok(find('#orderpad-modal #orderpad-summary').text().includes('TOTAL: 0.00'), "Correctly updates the total text");
   });
 
 });
@@ -102,15 +102,15 @@ test("Deleting an order item with quantity of 2 decrements to 1", function(asser
 
   assert.expect(3);
   visit('/orderpad/restaurant');
-  click('.orderpad-menu .list-group-item:first-child');
-  click('.orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
+  click('#orderpad-menu .list-group-item:first-child');
   click('#orderpad-order .orderpad-order .decrement-btn');
 
   andThen(function() {
 
     assert.equal(find('#orderpad-order .orderpad-order .list-group-item').length, 1, "Correctly removes menu item from main order item list");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
-    assert.ok(find('#orderpad-order .orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('ITEMS: 1'), "Correctly updates the item count text");
+    assert.ok(find('#orderpad-order #orderpad-summary').text().includes('TOTAL: 2.95'), "Correctly updates the total text");
   });
 
 });
