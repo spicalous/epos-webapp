5e4a7d1d9e2f3e8ff65cc4caca9891328ce1f466
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-26 00:51:38 +0100

    made menu height dynamic

---
+++ app/pods/orderpad/view.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  didInsertElement: function() {
+    $(window).resize(this.get('onResize'));
+    this.get('onResize')();
+  },
+  onResize: function() {
+    $('#orderpad-menu').height($(window).height() - $('#orderpad-categories').outerHeight());
+  }
+});
