c196a2102e1f1828b7e4261ca34ccd4f94120d10
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-26 19:22:18 +0100

    highlight currently selected category

--- app/pods/components/category-item/component.js
+++ app/pods/components/category-item/component.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
   attributeBindings: ['type'],
   type: 'button',
   classNames: ['col-xs-3', 'btn', 'btn-default', 'category-item'],
+  classNameBindings: ['active'],
+  selectedObserver: function() {
+    this.set('active', this.get('selected') === this.get('category'));
+  }.observes('selected'),
   click: function() {
     this.sendAction('action', this.get('category'));
   }
