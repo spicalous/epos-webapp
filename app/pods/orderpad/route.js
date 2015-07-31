a5147a05d587945b41a51074c9dce5226ea45462
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-07-31 20:22:05 +0100

    removed menu route

--- app/pods/orderpad/route.js
+++ app/pods/orderpad/route.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  renderTemplate: function() {
-    this.render('orderpad');
-    this.render('menuItem', {
-      into: 'orderpad',
-      outlet: 'menu',
-      controller: 'orderpad'
-    });
-  },
   model: function() {
     return Ember.RSVP.hash({
       menu: this.store.findAll('menu-item'),
