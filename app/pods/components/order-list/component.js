52a3e79a18841175097fb7b718e30bb0787afd5c
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-08-05 00:18:38 +0100

    increment order item

--- app/pods/components/order-list/component.js
+++ app/pods/components/order-list/component.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['orderpad-order', 'no-select'],
   actions: {
+    incrementItem: function(orderItem) {
+      orderItem.incrementProperty('quantity');
+    },
     decrementItem: function(orderItem) {
       if (orderItem.get('quantity') === 1) {
          orderItem.destroyRecord();
