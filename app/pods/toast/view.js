ca7350a0562efa252e3e94d3baa07772b51e97fc
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-08-13 10:03:50 +0100

    comment

--- app/pods/toast/view.js
+++ app/pods/toast/view.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+  /**
+  *
+  * Clears the timeout when another view replaces the toast
+  */
   willDestroyElement: function() {
     clearTimeout(this.get('controller.id'));
   }
