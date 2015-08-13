53a8e0ba2739100089e5f9089b07e0d01e5c57a1
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-08-13 23:34:41 +0100

    Deprecation: store.all to store.peekAll

--- app/pods/orderpad/route.js
+++ app/pods/orderpad/route.js
@@ -4,8 +4,8 @@ export default Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
       menu: this.store.findAll('menu-item'),
-      categories: this.store.all('category'),
-      editOptions: this.store.all('edit-option'),
+      categories: this.store.peekAll('category'),
+      editOptions: this.store.peekAll('edit-option'),
       order: this.store.createRecord('order', {})
     });
   },
