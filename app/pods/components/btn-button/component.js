d0398e6d7ee27152b98aa3f952dac668f7b63242
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-10-14 23:06:09 +0100

    moved more buttons to generic

--- app/pods/components/btn-button/component.js
+++ app/pods/components/btn-button/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'button',
-  attributeBindings: ['type'],
+  attributeBindings: ['type', 'data-toggle', 'data-target', 'data-dismiss'],
   type: 'button',
   classNameBindings: ['disabled'],
   classNames: ['btn'],
