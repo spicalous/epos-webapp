f31c27096215a357d68a78b5f0466d06636aa723
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-10-18 00:58:07 +0100

    look and feel

--- app/pods/components/order-list/component.js
+++ app/pods/components/order-list/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['orderpad-order', 'no-select'],
+  classNames: ['no-select'],
 
   actions: {
 
