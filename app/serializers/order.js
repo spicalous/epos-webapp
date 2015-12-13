6c57b4709bd5ba04058fefd381616ff2bbf16d25
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2015-12-13 23:45:44 +0000

    remove customer type from order serializer

--- app/serializers/order.js
+++ app/serializers/order.js
@@ -9,14 +9,5 @@ export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
     customer: {
       serialize: 'records'
     }
-  },
-  serialize(record) {
-    var json = this._super(...arguments);
-
-    json.customer.type = json.customerType;
-
-    delete json.customerType;
-
-    return json;
   }
 });
