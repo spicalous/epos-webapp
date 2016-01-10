a7cdef74aab661558bc76abee034a70bfed418f2
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-01-10 15:01:55 +0000

    display customer list

--- app/pods/customers/controller.js
+++ app/pods/customers/controller.js
@@ -26,14 +26,21 @@ export default Ember.Controller.extend({
       let contactNumber = this.get('searchContactNumber');
       let _this = this;
 
+      this.send('showMessage', 'loader', { message: 'Saving customer..' });
       this.store.query('delivery-customer', {
         address: address,
         postcode: postcode,
         contactNumber: contactNumber
       }).then(function(customers) {
+        _this.send('dismissMessage', 'loader');
         _this.set('deliveryCustomerResults', customers);
       }).catch(function(response) {
-
+        _this.send('dismissMessage', 'loader');
+        _this.set('debouncedSearch', '');
+        _this.send('showMessage', 'overlay', {
+          header: 'Error searching for customers :(',
+          body: response.errors[0].message
+        });
       });
     }
   }
