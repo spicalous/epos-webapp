c348c1d78f3615aa36b30902978edc40aacbf0e2
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-01-16 16:39:21 +0000

    updating customer details

--- app/pods/delivery-customer/controller.js
+++ app/pods/delivery-customer/controller.js
@@ -4,6 +4,15 @@ export default Ember.Controller.extend({
 
   editDisabled: Ember.computed.not('editable'),
 
+  saveDisabled: Ember.computed.not('canSave'),
+
+  canSave: Ember.computed.and('model.hasDirtyAttributes', 'hasNonEmptyFields'),
+
+  hasNonEmptyFields: Ember.computed('model.contactNumber', 'model.address', 'model.postcode', function() {
+    let model = this.get('model');
+    return !!model.get('contactNumber') && !!model.get('address') && !!model.get('postcode');
+  }),
+
   editable: false,
 
   actions: {
@@ -13,11 +22,18 @@ export default Ember.Controller.extend({
     },
 
     disableEdit() {
+      this.get('model').rollbackAttributes();
       this.set('editable', false);
     },
 
     saveEdit() {
+      let _this = this;
 
+      this.get('model').save().then(function() {
+        _this.set('editable', false);
+      }).catch(function() {
+
+      });
     }
   }
 
