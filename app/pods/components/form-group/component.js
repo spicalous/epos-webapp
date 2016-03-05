d3f91a7a5c11150fc9368101d677fc207b4c5f64
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-03-05 00:14:12 +0000

    adaptable form-group sizes

--- app/pods/components/form-group/component.js
+++ app/pods/components/form-group/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   classNameBindings: ['hasError'],
+
+  labelCol: 'col-sm-2',
+
+  inputCol: 'col-sm-9',
+
+  helpCol: 'col-sm-offset-2 col-sm-9',
+
 });
