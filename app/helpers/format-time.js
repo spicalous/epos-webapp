72c725a912ae45a5c37b64b3aad24c2e8c449f5b
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-03-09 21:41:50 +0000

    helper check

--- app/helpers/format-time.js
+++ app/helpers/format-time.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export function minus(params/*, hash*/) {
+  if (!params[0]) {
+    return '';
+  }
   if (typeof(params[0]) === 'string' || params[0] instanceof String) {
     params[0] = new Date(params[0]);
   }
