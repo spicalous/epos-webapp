d88db3f09a6ed2417c5dd5430f306d350d257938
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-03-20 01:10:12 +0000

    order totals on front end

--- app/pods/order/controller.js
+++ app/pods/order/controller.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  filterByPaymentMethod(orders, paymentMethod) {
+    return orders.filter(function(order) {
+      return order.get('paymentMethod') === paymentMethod;
+    });
+  },
+
+  totalCard: Ember.computed('model', function() {
+    let orders = this.get('model');
+    let cardOrders = this.filterByPaymentMethod(orders, 'CARD');
+
+    return cardOrders.reduce((prev, order) => prev + order.get('total'), 0);
+  }),
+
+  totalCash: Ember.computed('model', function() {
+    let orders = this.get('model');
+    let cashOrders = this.filterByPaymentMethod(orders, 'CASH');
+
+    return cashOrders.reduce((prev, order) => prev + order.get('total'), 0);
+  }),
+
+  totalNotPaid: Ember.computed('model', function() {
+    let orders = this.get('model');
+    let notPaidOrders = this.filterByPaymentMethod(orders, null);
+
+    return notPaidOrders.reduce((prev, order) => prev + order.get('total'), 0);
+  }),
+
+  totalAll: Ember.computed('model', function() {
+    let orders = this.get('model');
+
+    return orders.reduce((prev, order) => prev + order.get('total'), 0);
+  })
+
 });
