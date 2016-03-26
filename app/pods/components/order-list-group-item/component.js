481f767dd7fadf7903fe768455a31e4e75c149e8
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-03-26 15:24:28 +0000

    ability to reprint an order

--- app/pods/components/order-list-group-item/component.js
+++ app/pods/components/order-list-group-item/component.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['list-group-item']
+  classNames: ['list-group-item'],
+
+  actions: {
+    printOrder() {
+      let id = this.get('order.id');
+      this.get('onPrintOrder')(id);
+    }
+  }
 });
--- app/pods/order/controller.js
+++ app/pods/order/controller.js
@@ -2,29 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  filterByPaymentMethod(orders, paymentMethod) {
+  _filterByPaymentMethod(orders, paymentMethod) {
     return orders.filter(function(order) {
       return order.get('paymentMethod') === paymentMethod;
     });
   },
 
+  _getPrinterNamespaceURL() {
+    let host = this.store.adapterFor('application').get('host');
+    let namespace = this.store.adapterFor('application').get('namespace');
+    let url = [host, namespace, 'printer'].join('/');
+
+    return url;
+  },
+
   totalCard: Ember.computed('model', function() {
     let orders = this.get('model');
-    let cardOrders = this.filterByPaymentMethod(orders, 'CARD');
+    let cardOrders = this._filterByPaymentMethod(orders, 'CARD');
 
     return cardOrders.reduce((prev, order) => prev + order.get('total'), 0);
   }),
 
   totalCash: Ember.computed('model', function() {
     let orders = this.get('model');
-    let cashOrders = this.filterByPaymentMethod(orders, 'CASH');
+    let cashOrders = this._filterByPaymentMethod(orders, 'CASH');
 
     return cashOrders.reduce((prev, order) => prev + order.get('total'), 0);
   }),
 
   totalNotPaid: Ember.computed('model', function() {
     let orders = this.get('model');
-    let notPaidOrders = this.filterByPaymentMethod(orders, null);
+    let notPaidOrders = this._filterByPaymentMethod(orders, null);
 
     return notPaidOrders.reduce((prev, order) => prev + order.get('total'), 0);
   }),
@@ -33,6 +41,20 @@ export default Ember.Controller.extend({
     let orders = this.get('model');
 
     return orders.reduce((prev, order) => prev + order.get('total'), 0);
-  })
+  }),
+
+  actions: {
+
+    printOrder(id) {
+      let url = [this._getPrinterNamespaceURL(), 'order', id].join('/');
+      Ember.$.get(url).then();
+    },
+
+     printOrderSummary() {
+       let url = [this._getPrinterNamespaceURL(), 'order-summary'].join('/');
+       Ember.$.get(url).then();
+     }
+
+  }
 
 });
