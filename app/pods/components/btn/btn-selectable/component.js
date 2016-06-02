0644dad937d3df46d70e0331f0708f45c9782064
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-06-02 19:53:58 +0100

    css refactor

--- app/pods/components/btn/btn-selectable/component.js
+++ app/pods/components/btn/btn-selectable/component.js
@@ -3,7 +3,7 @@ import ButtonDefault from '../btn-default/component';
 
 export default ButtonDefault.extend({
 
-  classNameBindings: ['is-selected:btn-default-selected'],
+  classNameBindings: ['is-selected'],
 
   selectedObserver: Ember.observer('selected', function() {
     this.set('is-selected', this.get('selected') === this.get('model'));
