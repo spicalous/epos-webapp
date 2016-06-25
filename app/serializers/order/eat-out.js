02961660fd1653f3d8bdded2609734ec7278078f
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-06-25 16:32:43 +0100

    restore custom customer seialisation (ember data expects to receive "type" but sends "customerType"

--- app/serializers/order/eat-out.js
+++ app/serializers/order/eat-out.js
@@ -8,5 +8,17 @@ export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
     customer: {
       embedded: 'always'
     }
+  },
+  serialize(snapshot, options) {
+    var json = this._super(snapshot, options);
+
+    if (json.customer) {
+      if (json.customerType) {
+        json.customer.type = json.customerType;
+        delete json.customerType;
+      }
+    }
+
+    return json;
   }
 });
