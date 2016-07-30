24bf424bb7ca439b26b5f62061ebc2ae9bdee044
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-07-30 13:18:05 +0100

    Added boolean for removing customer

--- app/pods/components/inline-customer-input/component.js
+++ app/pods/components/inline-customer-input/component.js
@@ -4,6 +4,16 @@ export default Ember.Component.extend({
 
   classNames: ['inline-customer-input'],
 
+  canRemove: true,
+
+  /**
+   *  customer.constructor.modelName (store.createRecord)
+   *  customer.content.constructor.modelName (store.findRecord)
+   */
+  customerType: Ember.computed('customer', function() {
+    return this.get('customer.constructor.modelName') || this.get('customer.content.constructor.modelName');
+  }),
+
   actions: {
 
     selectTakeaway() {
