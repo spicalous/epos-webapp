f6efcad7ac2221ef58cea6642445e0464c652bfc
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-08-06 14:34:10 +0100

    unused code, and updated print order endpoints

--- app/models/order.js
+++ app/models/order.js
@@ -8,33 +8,5 @@ export default Model.extend({
   paymentMethod: attr('string', { defaultValue: null }),
   notes: attr('string'),
 
-  orderItems: hasMany('order-item'),
-
-  total: Ember.computed('orderItems.@each.quantity', 'orderItems.@each.total', function() {
-    return this.get('orderItems').reduce((prev, orderItem) => prev + orderItem.get('total'), 0);
-  }),
-
-  size: Ember.computed('orderItems.@each.quantity', function() {
-    return this.get('orderItems').reduce((prev, orderItem) => prev + orderItem.get('quantity'), 0);
-  }),
-
-  addItem(menuItem) {
-    let orderItems = this.get('orderItems');
-
-    let orderItem = orderItems.any(function(orderItem) {
-      if (orderItem.isMenuItem(menuItem) && orderItem.hasNoEditOptions()) {
-        return orderItem;
-      }
-    });
-
-    if (orderItem) {
-      orderItem.incrementProperty('quantity');
-    } else {
-      orderItems.pushObject(this.store.createRecord('order-item', {
-        quantity: 1,
-        menuItem: menuItem
-      }));
-    }
-  }
-
+  orderItems: hasMany('order-item')
 });
--- app/pods/components/order-list-group-item/component.js
+++ app/pods/components/order-list-group-item/component.js
@@ -17,6 +17,10 @@ export default Ember.Component.extend({
     return this.get('order.customer.constructor.modelName') || this.get('order.customer.content.constructor.modelName');
   }),
 
+  orderType: Ember.computed('order', function() {
+    return this.get('order.constructor.modelName');
+  }),
+
   actions: {
 
     editOrder() {
@@ -29,12 +33,16 @@ export default Ember.Component.extend({
 
     printOrderAsEatIn() {
       let id = this.get('order.id');
-      this.get('onPrintOrder')(id, ReceiptType.EAT_IN);
+      let orderType = this.get('orderType');
+
+      this.get('onPrintOrder')(id, orderType, ReceiptType.EAT_IN);
     },
 
     printOrderAsDelivery() {
       let id = this.get('order.id');
-      this.get('onPrintOrder')(id, ReceiptType.DELIVERY);
+      let orderType = this.get('orderType');
+
+      this.get('onPrintOrder')(id, orderType, ReceiptType.DELIVERY);
     }
   }
 });
