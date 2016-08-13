cbbbbe069651e0a0d01c54d4ac520f1081e14218
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-08-13 19:32:04 +0100

    revert customer model

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-  import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   telephone: attr('string', { defaultValue: '' }),
-
-  order: hasMany('order/eat-out'),
 
   invalidTelephone: Ember.computed('invalidTelephoneReason', function() {
     return !!this.get('invalidTelephoneReason');
