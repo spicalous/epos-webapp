ddf2efc99ed7b576ca49fbcd4f7912bb0b35bdfb
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-08-15 20:54:14 +0100

    allow selection of table to customer

--- server/mocks/tables.js
+++ server/mocks/tables.js
@@ -2,110 +2,11 @@ module.exports = function(app) {
   var express = require('express');
   var tablesRouter = express.Router();
 
+  var tablesArray = require('../json/tables.json');
+
   tablesRouter.get('/', function(req, res) {
     res.send({
-      'table': [
-        {
-          'id': 0,
-          'tableId': '1',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 1,
-          'tableId': '2',
-          'status': 'OCCUPIED'
-        },
-        {
-          'id': 2,
-          'tableId': '3',
-          'status': 'PAYMENT_PENDING'
-        },
-        {
-          'id': 3,
-          'tableId': '4',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 4,
-          'tableId': '5',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 5,
-          'tableId': '6',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 6,
-          'tableId': '7',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 7,
-          'tableId': '8',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 8,
-          'tableId': '9',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 9,
-          'tableId': '10',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 10,
-          'tableId': '11',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 11,
-          'tableId': '12',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 12,
-          'tableId': '13',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 13,
-          'tableId': '14',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 14,
-          'tableId': '15',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 15,
-          'tableId': '16',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 16,
-          'tableId': '17',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 17,
-          'tableId': '18',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 18,
-          'tableId': '19',
-          'status': 'AVAILABLE'
-        },
-        {
-          'id': 19,
-          'tableId': '20',
-          'status': 'AVAILABLE'
-        }
-      ]
+      'tables': tablesArray
     });
   });
 
