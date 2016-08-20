5fa5f2abeb334517a12598ced06051eb55c606fe
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-08-20 13:43:37 +0100

    added simple startsWith polyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -37,5 +37,7 @@ module.exports = function(defaults) {
   });
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
+  app.import('vendor/polyfill.js', { prepend: true });
+
   return app.toTree();
 };
