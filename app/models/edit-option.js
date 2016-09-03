45b04bd72b89af5472d39b2dd1131962c76e7664
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2016-09-04 00:39:36 +0100

    DRY format currency

--- app/models/edit-option.js
+++ app/models/edit-option.js
@@ -5,11 +5,5 @@ import attr from 'ember-data/attr';
 export default Model.extend({
   editCategory: attr('number'),
   name: attr('string'),
-  price: attr('number'),
-
-  displayPrice: Ember.computed('price', function() {
-    let price = this.get('price');
-    return price === 0 ? '' : (price / 100).toFixed(2);
-  })
-
+  price: attr('number')
 });
