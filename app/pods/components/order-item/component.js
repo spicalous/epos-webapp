9989b750d933356e7977a64ee527cd13b78a5b10
benlert <ben.lertlumprasertkul@gmail.com>
2017-06-29 00:50:57 +0100

    edit dialog

--- app/pods/components/order-item/component.js
+++ app/pods/components/order-item/component.js
@@ -10,6 +10,13 @@ export default Ember.Component.extend({
 
   editing: false,
 
+  selectedEditCategoryId: null,
+
+  filteredEditOptions: Ember.computed('selectedEditCategoryId', function() {
+    let selectedEditCategoryId = this.get('selectedEditCategoryId');
+    return this.get('editOptions').filterBy('editCategoryId', parseInt(selectedEditCategoryId));
+  }),
+
   click(event) {
     // only toggle expanded if click events we're not from a modal when editing an item
     if (this.$(event.target).closest('.modal').length === 0) {
@@ -28,7 +35,15 @@ export default Ember.Component.extend({
     },
 
     toggleEdit() {
+      this.set('selectedEditCategoryId', this.get('editing') ?
+        null :
+        this.get('item.menuItem.editCategories.firstObject.id'));
+
       this.toggleProperty('editing');
+    },
+
+    setSelectedEditCategory(editCategory) {
+      this.set('selectedEditCategoryId', editCategory.get('id'));
     }
 
   }
--- app/pods/order/edit/create/route.js
+++ app/pods/order/edit/create/route.js
@@ -4,6 +4,11 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.createRecord('order');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('menu', this.modelFor('order/edit'));
   }
 
 });
